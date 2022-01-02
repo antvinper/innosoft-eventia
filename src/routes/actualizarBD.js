f14889406f9342b01a7a6cc371aa7e4944c3c714
Antonio <antvinper@alum.us.es>
2022-01-02 20:48:52 +0100

    Task#42 - Modificar botones de API de Twitter
    El botón ahora depende de si el evento ha sido previamente publicado en Twitter. Solucionado además un error con las urls de ciertas imágenes.

--- src/routes/actualizarBD.js
+++ src/routes/actualizarBD.js
@@ -29,7 +29,8 @@ router.put('/', async(req, res) => {
                     fin: new Date(evento.end.local),
                     imagen: evento.logo.original.url,
                     botonGmail: false,
-                    publicadoFacebook: false
+                    publicadoFacebook: false,
+                    publicadoTwitter: false
                 }
                 añadirBD.push(peticionPublicacion)
             }
